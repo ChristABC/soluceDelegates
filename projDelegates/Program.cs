70c7759b4945a4832cd619c1926479a82766960c
Christ ASSANA <christassana@gmail.com>
2024-07-01 18:17:34 +0200

    Ajoutez des fichiers projet.

---
+++ projDelegates/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using projDelegates;
+
+namespace ShowMoney
+{
+    class Program
+    {
+        // Définition de la déléguée ShowMoney
+        delegate void ShowMoney(float amount);
+
+        static void Main(string[] args)
+        {
+            float amount = 12.15f ;// On ajoute le 'f' pour dire qu'on a un nombre flottant
+
+            // Instanciation de la déléguée avec la méthode DisplayInDollars de MoneyDisplayer
+            ShowMoney showMoney = MoneyDisplayer.DisplayInDollars;
+            showMoney(amount);
+
+            // Réaffectation de la déléguée avec la méthode DisplayInEuros de MoneyDisplayer
+            showMoney = MoneyDisplayer.DisplayInEuros;
+            showMoney(amount);
+        }
+    }
+}
---
+++ projDelegates/ShowMoney.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projDelegates
+{
+    public static class MoneyDisplayer
+    {
+        // Méthode pour afficher en dollars
+        public static void DisplayInDollars(float amount)
+        {
+            Console.WriteLine($"{amount:C2} $");
+        }
+
+        // Méthode pour afficher en euros
+        public static void DisplayInEuros(float amount)
+        {
+            Console.WriteLine($"{amount:C2} €");
+        }
+    }
+}
+
+
